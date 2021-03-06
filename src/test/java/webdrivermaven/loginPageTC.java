666c34f07ee7a35cded5c4601bbe004afaadae54
Sudheer <sudheer2005@gmail.com>
2021-03-06 07:26:13 +0100

    Inital Commit

---
+++ src/test/java/webdrivermaven/loginPageTC.java
@@ -0,0 +1,33 @@
+package webdrivermaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class loginPageTC {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void doLogin() {
+		
+		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=googlemail&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+		driver.findElement(By.id("identifierId")).sendKeys("sudheer1234@gmail.com");
+
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
